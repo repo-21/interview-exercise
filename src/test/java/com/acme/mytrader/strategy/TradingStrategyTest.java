05ef7ad2ca3d7e77162e41ec5c4a9b14d241da63
todip <punit.todi+java1@gmail.com>
2022-03-28 21:36:17 +0100

    PT-1: add updated solution

--- src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
+++ src/test/java/com/acme/mytrader/strategy/TradingStrategyTest.java
@@ -3,6 +3,7 @@ package com.acme.mytrader.strategy;
 import com.acme.mytrader.execution.ExecutionService;
 import com.acme.mytrader.price.PriceListener;
 import com.acme.mytrader.price.PriceSource;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -19,7 +20,7 @@ public class TradingStrategyTest {
     }
 
     @Test
-    public void testStrategy() {
+    public void testStrategyBUY() {
         ExecutionService executionService = Mockito.mock(ExecutionService.class);
         doNothing().when(executionService).buy("IBM", 55, 100);
         PriceSourceImpl priceSource = new PriceSourceImpl();
@@ -31,10 +32,25 @@ public class TradingStrategyTest {
         verify(executionService, times(1)).buy("IBM", 55, 100);
     }
 
+    @Test
+    public void testStrategySELL() {
+        ExecutionService executionService = Mockito.mock(ExecutionService.class);
+        doNothing().when(executionService).sell("IBM", 100, 100);
+        PriceSourceImpl priceSource = new PriceSourceImpl();
+
+        TradingStrategy tradingStrategy = new TradingStrategy(executionService, priceSource);
+
+        priceSource.priceEvent("IBM", 101);
+
+        verify(executionService, times(1)).sell("IBM", 100, 100);
+    }
+
     // This impl isn't expected, but I wasn't able to trigger orders without it
     // so a simplest impl is provided to enable this tests
+    @Ignore
     public static class PriceSourceImpl implements PriceSource {
         Set<PriceListener> listeners = new HashSet<>();
+
         @Override
         public void addPriceListener(PriceListener listener) {
             listeners.add(listener);
